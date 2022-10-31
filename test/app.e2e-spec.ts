d11938f69606e9a95d4964523ec455f74ea59c45
Zack Bello <obomheire@gmail.com>
2022-10-31 10:50:08 +0100

    2 test passed

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -67,40 +67,41 @@ describe('App e2e', () => {
           .post('/auth/signup')
           .withBody(dto)
           .expectStatus(201);
+        // .inspect();
       });
     });
 
-    // describe('Signin', () => {
-    //   it('should throw if email empty', () => {
-    //     return pactum
-    //       .spec()
-    //       .post('/auth/signin')
-    //       .withBody({
-    //         password: dto.password,
-    //       })
-    //       .expectStatus(400);
-    //   });
-    //   it('should throw if password empty', () => {
-    //     return pactum
-    //       .spec()
-    //       .post('/auth/signin')
-    //       .withBody({
-    //         email: dto.email,
-    //       })
-    //       .expectStatus(400);
-    //   });
-    //   it('should throw if no body provided', () => {
-    //     return pactum.spec().post('/auth/signin').expectStatus(400);
-    //   });
-    //   it('should signin', () => {
-    //     return pactum
-    //       .spec()
-    //       .post('/auth/signin')
-    //       .withBody(dto)
-    //       .expectStatus(200)
-    //       .stores('userAt', 'access_token');
-    //   });
-    // });
+    describe('Signin', () => {
+      //   it('should throw if email empty', () => {
+      //     return pactum
+      //       .spec()
+      //       .post('/auth/signin')
+      //       .withBody({
+      //         password: dto.password,
+      //       })
+      //       .expectStatus(400);
+      //   });
+      //   it('should throw if password empty', () => {
+      //     return pactum
+      //       .spec()
+      //       .post('/auth/signin')
+      //       .withBody({
+      //         email: dto.email,
+      //       })
+      //       .expectStatus(400);
+      //   });
+      //   it('should throw if no body provided', () => {
+      //     return pactum.spec().post('/auth/signin').expectStatus(400);
+      //   });
+      it('should signin', () => {
+        return pactum
+          .spec()
+          .post('/auth/signin')
+          .withBody(dto)
+          .expectStatus(200);
+        // .stores('userAt', 'access_token');
+      });
+    });
   });
 
   // describe('User', () => {
