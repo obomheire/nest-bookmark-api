3f81e23c33d649c737884b8b3858f43e5b686969
Zack Bello <obomheire@gmail.com>
2022-11-03 15:11:08 +0100

    Added comment

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { PrismaModule } from 'src/prisma/prisma.module';
 // import { PrismaModule } from 'src/prisma/prisma.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtStrategy } from './strategy';
 
 @Module({
-  // imports: [PrismaModule],
+  // imports: [PrismaModule], // We don't need this because we make the PrismaModule @Global()
   imports: [JwtModule.register({})],
   controllers: [AuthController],
   providers: [AuthService, JwtStrategy],
